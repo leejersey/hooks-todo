cc2d20e95b5493883534754f2f274cb66ecb1e46
lizexi <Zexi.Li@b-and-qchina.com>
2020-05-18 17:06:44 +0800

    fix

--- src/Todolist.js
+++ src/Todolist.js
@@ -26,12 +26,12 @@ const Todolist= memo(function Todolist() {
      },[])
 
      useEffect(() => {
-        const todos = JSON.parse(Window.localStorage.getItem('todos')) || [];
+        const todos = JSON.parse(window.localStorage.getItem('todos')) || [];
         setTodos(todos);
     },[])
 
     useEffect(()=> {
-        Window.localStorage.setItem('todos',JSON.stringify(todos))
+        window.localStorage.setItem('todos',JSON.stringify(todos))
     },[todos])
   return ( 
     <div>
